e6b42ebd4a11f165d7cca225084d2eb283622d98
Dimas Sudjito <almuhtad@ualberta.ca>
2021-07-01 22:06:28 -0600

    clean up project

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,7 +1,42 @@
 <template>
-  <v-row>
+  <!-- <v-row>
     <v-col class="text-center">
-      <p>Check</p>
+      <p>Check the source code here</p>
+      <p>
+        <a target="_blank" href="https://github.com/dimassudjito/titanic-client"
+          >Client</a
+        >
+      </p>
+      <p>
+        <a target="_blank" href="https://github.com/dimassudjito/titanic-flask"
+          >Flask API</a
+        >
+      </p>
+    </v-col>
+  </v-row> -->
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Check the source code here
+        </v-card-title>
+        <v-card-subtitle class="mt-4">
+          <p>
+            <a
+              target="_blank"
+              href="https://github.com/dimassudjito/titanic-client"
+              >Client</a
+            >
+          </p>
+          <p>
+            <a
+              target="_blank"
+              href="https://github.com/dimassudjito/titanic-flask"
+              >Flask API</a
+            >
+          </p>
+        </v-card-subtitle>
+      </v-card>
     </v-col>
   </v-row>
 </template>
